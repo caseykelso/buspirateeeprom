62df7e797e0046f885ec88f3f045fa017ad2b642
caseykelso <casey@caseykelso.com>
2017-11-17 12:56:26 -0600

    output verifies

--- pirate/i2c-write.py
+++ pirate/i2c-write.py
@@ -12,9 +12,11 @@ from array import *
 
 mydata = array('B', [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0])
 
+def bytes(integer):
+    return divmod(integer, 0x100)
+
 def i2c_write_byte(address, data):
-    haddress = address >> 8;
-    laddress = address&0xff
+    haddress, laddress = bytes(address)
     global mydata
 #    i2c.send_start_bit()
 #    i2c.bulk_trans(1, [0xa2])
@@ -23,14 +25,15 @@ def i2c_write_byte(address, data):
 #    i2c.bulk_trans(1, [ord(data)])
 #    print "data: %s" % hex(ord(data))
 #    print "address: %s" % hex(address)
-    if ((laddress % 16)) == 0 and (laddress != 0):
-        print '%04x%04x  %02x %02x %02x %02x %02x %02x %02x %02x  %02x %02x %02x %02x %02x %02x %02x %02x' % (haddress,laddress-16,mydata[0],mydata[1],mydata[2],mydata[3],mydata[4],mydata[5],mydata[6],mydata[7],mydata[8],mydata[9],mydata[10],mydata[11],mydata[12],mydata[13],mydata[14],mydata[15])
+    if ((laddress % 16)) == 0 and (address != 0):
+        print '%07x %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o %03o' % (address-16,mydata[0],mydata[1],mydata[2],mydata[3],mydata[4],mydata[5],mydata[6],mydata[7],mydata[8],mydata[9],mydata[10],mydata[11],mydata[12],mydata[13],mydata[14],mydata[15])
         mydata[laddress % 16] = ord(data)
 #    elif laddress == 0:
 #        mydata[0] = ord(data)
 #        print 'yo'
     else:
         mydata[laddress % 16] = ord(data)
+#        print 'yo'
 #    i2c.send_stop_bit()
 
 
@@ -66,6 +69,7 @@ if __name__ == '__main__':
    # Start dumping
 #    for block in range(0, args.size, args.bsize):
     i = 0
+    print args.size
     for i in range(0, args.size):
 #        print i
         i2c_write_byte(i, args.inputfile.read(1))
